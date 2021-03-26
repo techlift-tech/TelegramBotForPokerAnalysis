a6dd6f1ca4078f308b353f8d688afb5d4b1e9f29
disha-2401 <dishamiyani03@gmail.com>
2021-03-26 15:23:37 +0530

    Add project files.

---
+++ TechliftTelegramBot/Controllers/TelegramBot.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+using Newtonsoft.Json;
+using System.Web;
+using TechliftTelegramBot.Models;
+
+namespace TechliftTelegramBot.Controllers
+{
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class telegramBot : ControllerBase
+    {
+        //botClient = new TelegramBotClient("1770785118:AAF6d9W3xbI1G4n-EW4Nz4MiK6BgdjM9EN0");
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("hello world");
+        }
+
+        [HttpPost]
+        public IActionResult Post(JObject payload)
+        {
+            var privatechat = payload.ToObject<NewMessage>();
+            var botClient = new TelegramBotClient("1770785118:AAF6d9W3xbI1G4n-EW4Nz4MiK6BgdjM9EN0");
+            var me = botClient.GetMeAsync().Result;
+            Console.WriteLine(
+              $"Hello, World! I am user {me.Username} and my name is {me.FirstName}.");
+            if (privatechat.Message != null)
+            {
+                if (privatechat.Message.Text == "/hello")
+                {
+                    botClient.SendTextMessageAsync(
+                        chatId: privatechat.Message.Chat.Id,
+                        text: "hello " + privatechat.Message.From.FirstName);
+                }
+               
+                else if (privatechat.Message.Text == "tell me time")
+                {
+                    botClient.SendTextMessageAsync(
+                        chatId: privatechat.Message.Chat.Id,
+                        text: "Current time is = " + DateTime.Now.ToString("h:mm:ss tt"));
+                }
+           
+                else if (privatechat.Message.Text != null)
+                {
+                    if (privatechat.Message.Text.StartsWith("/"))
+                    {
+                        botClient.SendTextMessageAsync(
+                            chatId: privatechat.Message.Chat.Id,
+                            text: "hello " + privatechat.Message.From.FirstName + " you said \n" + privatechat.Message.Text);
+                    }
+                    return Ok(payload.ToString());
+                }
+            }
+            return Ok(payload.ToString());
+        }
+    }
+}
---
+++ TechliftTelegramBot/Models/NewMessage.cs
@@ -0,0 +1,74 @@
+﻿namespace TechliftTelegramBot.Models
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
+    public partial class NewMessage
+    {
+        [JsonProperty("update_id")]
+        public long UpdateId { get; set; }
+
+        [JsonProperty("message")]
+        public Message Message { get; set; }
+    }
+
+    public partial class Message
+    {
+        [JsonProperty("message_id")]
+        public long MessageId { get; set; }
+
+        [JsonProperty("from")]
+        public From From { get; set; }
+
+        [JsonProperty("chat")]
+        public Chat Chat { get; set; }
+
+        [JsonProperty("date")]
+        public long Date { get; set; }
+
+        [JsonProperty("text")]
+        public string Text { get; set; }
+    }
+
+    public partial class Chat
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+    }
+
+    public partial class From
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("is_bot")]
+        public bool IsBot { get; set; }
+
+        [JsonProperty("first_name")]
+        public string FirstName { get; set; }
+
+        [JsonProperty("last_name")]
+        public string LastName { get; set; }
+
+        [JsonProperty("username")]
+        public string Username { get; set; }
+
+        [JsonProperty("language_code")]
+        public string LanguageCode { get; set; }
+    }
+}
